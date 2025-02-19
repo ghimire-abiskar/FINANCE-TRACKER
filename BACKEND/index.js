7c38f4977cc6169ff86c05af54767e7342405535
Abiskar <ghimirra@gmail.com>
2025-02-20 00:10:59 +0530

    Added rate limiters

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -1,21 +1,30 @@
-const express =require('express');
-const connectToMongo=require('./db')
-const cors=require('cors')
-const mongoURI ='mongodb://localhost:27017/finance_tracker';
-const app=express();
-const authRoutes=require('./routes/auth')
-const transRoutes=require('./routes/transaction')
-const port=5001;
+const express = require('express');
+const connectToMongo = require('./db')
+const cors = require('cors')
+const app = express();
+const authRoutes = require('./routes/auth')
+const transRoutes = require('./routes/transaction')
+const rateLimit = require('express-rate-limit');
+const port = 5001;
 app.use(express.json())
 app.use(cors());
 
-app.get('/',(req,res)=>{
+const limiter = rateLimit({
+    max: 3,
+    windowMs: 20 * 1000,
+    message: "Too many requests!!!"
+})
+
+app.get('/', (req, res) => {
     res.send("Hey I am here!!");
 })
-app.use('/api/auth',authRoutes);
-app.use('/api/trans',transRoutes);
 
-app.listen(port,()=>{
+app.use('/api', limiter);
+
+app.use('/api/auth', authRoutes);
+app.use('/api/trans', transRoutes);
+
+app.listen(port, () => {
     console.log(`Server is running in the port ${port}`)
 })
 
